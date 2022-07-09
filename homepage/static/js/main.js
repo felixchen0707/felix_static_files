de80362fb9770e8ed5300a999fb6d67bca906f68
https://gitee.com/felixchen0707 <3137181992@qq.com>
2022-07-09 12:59:34 +0800

    修正js

--- homepage/static/js/main.js
+++ homepage/static/js/main.js
@@ -1,1 +1,1 @@
-console.log("\n %c 吃草莓呀!一直爱着所有人~ %c by chrelyonly | chrelyonly.cn ","color:#444;background:#eee;padding:5px 0;","color:#F8F8FF;background:#F4A7B9;padding:5px 0;");
+console.log("\n %c 感谢大家不断的支持！ %c by Felix_Chen | felixchen0707.cn ","color:#444;background:#eee;padding:5px 0;","color:#F8F8FF;background:#F4A7B9;padding:5px 0;");
